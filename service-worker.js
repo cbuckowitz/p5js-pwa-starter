839330d5a81ede4414cd16dfcd8b73b211f2e4e4
Christian Buckowitz <cbuckowitz@gmail.com>
2020-06-18 22:26:27 +0000

    pwa manifest and service worker for offline files

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const STATIC_CACHE_NAME = "static-cache-v1";
+
+const STATIC_CACHE_FILES = [
+  "/",
+  "/index.html",
+  "/scripts/sketch.js",
+  "/lib/p5.min.js",
+  "/styles/style.css"
+];
+
+self.addEventListener("install", evt => {
+  evt.waitUntil(
+    caches.open(STATIC_CACHE_NAME).then(cache => cache.addAll(STATIC_CACHE_FILES))
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== STATIC_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", evt => {
+  evt.respondWith(
+    caches.open(STATIC_CACHE_NAME).then(cache => {
+      return cache.match(evt.request).then(response => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
